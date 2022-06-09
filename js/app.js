a0c896c4aeee8b52cfb68fca54508690f798f552
Jibon <3djibon1@gmail.com>
2022-06-10 00:08:28 +0600

    v-1.1 done

---
+++ js/app.js
@@ -0,0 +1,32 @@
+(function ($) {
+    "use strict";
+
+     // youtub player
+     $(".P1").YTPlayer({
+        showControls: false,
+        mute: true,
+        autoPlay: true,
+        loop: true,
+        ratio: "16/9",
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})(jQuery);
